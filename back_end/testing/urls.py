9f85d64a2c873864f28bde319bd7a8a6f7e9dfcb
syamvelaga <syamvelaga64@gmail.com>
2023-08-26 20:38:22 +0530

    Creat table PersonalDetals

--- back_end/testing/urls.py
+++ back_end/testing/urls.py
@@ -6,6 +6,7 @@ app_name = "testing"
 
 urlpatterns = [
     path('register/', views.RegisterUser.as_view(), name='register'),
+    path('personal_details/', views.PersonalDetails.as_view(), name="personal_details"),
     # path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
 ]
--- back_end/testing/views.py
+++ back_end/testing/views.py
@@ -31,6 +31,44 @@ class RegisterUser(APIView):
             return HttpResponseServerError(error_message)
 
 
+class PersonalDetails(APIView):
+    def post(self,request):
+        print(request.data)
+        FamilyName = request.data["family_name"]
+        Name = request.data["name"]
+        Dob = request.data["dob"]
+        EmailId = request.data["email_id"]
+        AlterEmailId = request.data["alter_email_id"]
+        PhoneNumber = request.data["phone_number"]
+
+        data=(FamilyName,Name,Dob,EmailId,AlterEmailId,PhoneNumber)
+
+        try:
+            query = """ insert into PersonalDetails (FamilyName, Name, Dob,EmailId,AlterEmailId,PhoneNumber) 
+            values(%s, %s, %s,%s,%s,%s)"""
+            
+            with connection.cursor() as cursor:
+                cursor.execute(query,data)
+                cursor.close()
+            
+            return JsonResponse({'message': "PersonalDetails successfully Inserted"}, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            error_message = f"An unexpected error occurred: {e}"
+            return HttpResponseServerError(error_message)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def user_logout(request):
